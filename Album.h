264e324ef4829ea06f8317d7184159c978bd75ae
marianafriass <a01709125@tec.mx>
2025-05-22 08:36:17 -0600

    Create Album.h

---
+++ Album.h
@@ -0,0 +1,30 @@
+#ifndef ALBUM_H
+#define ALBUM_H
+
+#include "ElementoMusical.h"
+#include "Artista.h"
+
+class Album : public ElementoMusical {
+private:
+    string nombreAlbum;
+    int numCanciones;
+    Artista* artista;
+
+public:
+    Album();
+    Album(string titulo, int duracion, int anio, string nombreAlbum, int numCanciones);
+    
+    // Sobrescritura 
+    void reproducir() override;
+    string obtenerInfo() override;
+
+    // Getters y setters
+    string getNombreAlbum() const;
+    void setNombreAlbum(string nombreAlbum);
+    int getTotalCanciones() const;
+    void setTotalCanciones(int numCanciones);
+    Artista* getArtista() const;
+    void setArtista(Artista* artista);
+};
+
+#endif
