6a156521894f5183805870dc31d77835c80760c4
marianafriass <a01709125@tec.mx>
2025-05-22 08:37:53 -0600

    Create Album.cpp

---
+++ Album.cpp
@@ -0,0 +1,46 @@
+#include "Album.h"
+#include <iostream>
+using namespace std;
+
+Album::Album()
+    : ElementoMusical(), nombreAlbum(""), numCanciones(0), artista(nullptr) {}
+
+Album::Album(string titulo, int duracion, int anio,
+             string nombreAlbum, int numCanciones)
+    : ElementoMusical(titulo, duracion, anio),
+      nombreAlbum(nombreAlbum),
+      numCanciones(numCanciones),
+      artista(nullptr) {}
+
+void Album::reproducir() {
+    cout << "Reproduciendo álbum: " << nombreAlbum << endl;
+}
+
+string Album::obtenerInfo() {
+    return "Álbum: " + nombreAlbum +
+           " (" + to_string(numCanciones) + " canciones)";
+}
+
+string Album::getNombreAlbum() const {
+    return nombreAlbum;
+}
+
+void Album::setNombreAlbum(string nombreAlbum) {
+    this->nombreAlbum = nombreAlbum;
+}
+
+int Album::getTotalCanciones() const {
+    return numCanciones;
+}
+
+void Album::setTotalCanciones(int numCanciones) {
+    this->numCanciones = numCanciones;
+}
+
+Artista* Album::getArtista() const {
+    return artista;
+}
+
+void Album::setArtista(Artista* artista) {
+    this->artista = artista;
+}
