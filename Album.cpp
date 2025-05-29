80bf6500c8cd00ef3217a04b817baf2f08ffd4bb
marianafriass <a01709125@tec.mx>
2025-05-29 08:04:51 -0600

    Update Album.cpp

--- Album.cpp
+++ Album.cpp
@@ -1,46 +1,24 @@
 #include "Album.h"
 #include <iostream>
-using namespace std;
 
-Album::Album()
-    : ElementoMusical(), nombreAlbum(""), numCanciones(0), artista(nullptr) {}
+Album::Album() : ElementoMusical(), nombreAlbum(""), numCanciones(0), artista(nullptr) {}
 
-Album::Album(string titulo, int duracion, int año,
-             string nombreAlbum, int numCanciones)
-    : ElementoMusical(titulo, duracion, año),
-      nombreAlbum(nombreAlbum),
-      numCanciones(numCanciones),
-      artista(nullptr) {}
+Album::Album(string titulo, int duracion, int año, string nombreAlbum, int numCanciones)
+    : ElementoMusical(titulo, duracion, año), nombreAlbum(nombreAlbum), 
+      numCanciones(numCanciones), artista(nullptr) {}
 
 void Album::reproducir() {
-    cout << "Reproduciendo álbum: " << nombreAlbum << endl;
+    cout << "Reproduciendo álbum: " << getTitulo() << " (" << numCanciones << " canciones)" << endl;
 }
 
 string Album::obtenerInfo() {
-    return "Álbum: " + nombreAlbum +
+    return "Álbum: " + getTitulo() + " - " + nombreAlbum + 
            " (" + to_string(numCanciones) + " canciones)";
 }
 
-string Album::getNombreAlbum() const {
-    return nombreAlbum;
-}
-
-void Album::setNombreAlbum(string nombreAlbum) {
-    this->nombreAlbum = nombreAlbum;
-}
-
-int Album::getTotalCanciones() const {
-    return numCanciones;
-}
-
-void Album::setTotalCanciones(int numCanciones) {
-    this->numCanciones = numCanciones;
-}
-
-Artista* Album::getArtista() const {
-    return artista;
-}
-
-void Album::setArtista(Artista* artista) {
-    this->artista = artista;
-}
+string Album::getNombreAlbum() const { return nombreAlbum; }
+void Album::setNombreAlbum(string nombreAlbum) { this->nombreAlbum = nombreAlbum; }
+int Album::getTotalCanciones() const { return numCanciones; }
+void Album::setTotalCanciones(int numCanciones) { this->numCanciones = numCanciones; }
+Artista* Album::getArtista() const { return artista; }
+void Album::setArtista(Artista* artista) { this->artista = artista; }
