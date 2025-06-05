f5ab6ada75e1ad305f50ddf8d8846d2d3504d395
marianafriass <a01709125@tec.mx>
2025-06-05 07:14:07 -0600

    Update Cancion.cpp

--- Cancion.cpp
+++ Cancion.cpp
@@ -1,50 +1,28 @@
 #include "Cancion.h"
 #include <iostream>
-using namespace std;
 
-Cancion::Cancion()
-    : ElementoMusical(),
-      genero(""),
-      nombreArtista(""),
-      artista(nullptr) {}
+Cancion::Cancion() : ElementoMusical(), genero(""), nombreArtista(""), artista(nullptr) {}
 
-Cancion::Cancion(string titulo, int duracion, int año,
-                 string genero, string nombreArtista)
-    : ElementoMusical(titulo, duracion, año),
-      genero(genero),
-      nombreArtista(nombreArtista),
-      artista(nullptr) {}
+Cancion::Cancion(string titulo, int duracion, int anio, string genero, string nombreArtista)
+    : ElementoMusical(titulo, duracion, anio), genero(genero), nombreArtista(nombreArtista), artista(nullptr) {}
 
-void Cancion::reproducir() {
-    cout << "Reproduciendo canción: "
-         << getTitulo() << " - " << nombreArtista << endl;
-}
+      void Cancion::reproducir() {
+          cout << "Reproduciendo canción: " << getTitulo() << " - " << nombreArtista << endl;
+      }
 
-string Cancion::obtenerInfo() {
-    return "Canción: " + getTitulo() +
-           " (" + genero + ") por " + nombreArtista;
-}
+      string Cancion::obtenerInfo() {
+          return "Canción: " + getTitulo() + " (" + genero + ") por " + nombreArtista;
+      }
 
 string Cancion::getGenero() const {
-    return genero;
-}
-
+    return genero; }
 void Cancion::setGenero(string genero) {
-    this->genero = genero;
-}
-
+    this->genero = genero; }
 string Cancion::getNombreArtista() const {
-    return nombreArtista;
-}
-
+    return nombreArtista; }
 void Cancion::setNombreArtista(string nombreArtista) {
-    this->nombreArtista = nombreArtista;
-}
-
+    this->nombreArtista = nombreArtista; }
 Artista* Cancion::getArtista() const {
-    return artista;
-}
-
+    return artista; }
 void Cancion::setArtista(Artista* artista) {
-    this->artista = artista;
-}
+    this->artista = artista; }
