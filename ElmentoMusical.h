5889adb53b75808733452735c9b1cf91c57dd487
marianafriass <a01709125@tec.mx>
2025-06-05 07:19:25 -0600

    Create ElmentoMusical.h

---
+++ ElmentoMusical.h
@@ -0,0 +1,31 @@
+#ifndef ELEMENTOMUSICAL_H
+#define ELEMENTOMUSICAL_H
+
+#include <string>
+using namespace std;
+
+class ElementoMusical {
+protected:
+    string titulo;
+    int duracion;
+    int anio;
+
+public:
+    ElementoMusical();
+    ElementoMusical(string titulo, int duracion, int anio);
+    virtual ~ElementoMusical() = default;
+
+    // Métodos virtuales puros
+    virtual void reproducir() = 0;
+    virtual string obtenerInfo() = 0;
+
+    // Getters y setters
+    string getTitulo() const;
+    void setTitulo(string titulo);
+    int getDuracion() const;
+    void setDuracion(int duracion);
+    int getAnio() const;
+    void setAnio(int anio);
+};
+
+#endif
