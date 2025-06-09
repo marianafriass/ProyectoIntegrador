9e61ca2052b896ce22a7dab05ce5aea66c9096d7
marianafriass <a01709125@tec.mx>
2025-06-09 01:40:57 -0600

    Update Artista.h

--- Artista.h
+++ Artista.h
@@ -2,7 +2,6 @@
 #define ARTISTA_H
 
 #include <string>
-#include <ostream>
 using namespace std;
 
 class Artista {
@@ -14,16 +13,16 @@ public:
     Artista();
     Artista(string nombre, string genero);
     
-    // Operadores sobrecargados
-    bool operator==(const Artista& otro) const;
-    bool operator!=(const Artista& otro) const;
-    friend ostream& operator<<(ostream& os, const Artista& artista);
-    
+    // Métodos
     string getNombre() const;
     void setNombre(string nombre);
     string getGenero() const;
     void setGenero(string genero);
     string mostrarInfo() const;
+
+    // Operadores sobrecargados eliminados
+    bool operator==(const Artista& otro) const;
+    bool operator!=(const Artista& otro) const;
 };
 
 #endif
