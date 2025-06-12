5a0f86ee3b1a4b40757bb4c2ea73e8a9166d554a
marianafriass <a01709125@tec.mx>
2025-06-12 03:04:45 -0600

    Update ElementoMusical.h

--- ElementoMusical.h
+++ ElementoMusical.h
@@ -12,15 +12,15 @@ protected:
 
 public:
     ElementoMusical(string titulo, int duracion, int anio);
-
+    //getters
     string getTitulo();
     int getDuracion();
     int getAnio();
-
+    //Setters
     void setTitulo(string titulo);
     void setDuracion(int duracion);
     void setAnio(int anio);
-
+    //implementacion de los metodos virtuales
     virtual void reproducir() = 0;
     virtual string obtenerInfo() = 0;
 };
