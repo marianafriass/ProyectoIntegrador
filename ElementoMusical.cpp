a41c256b4872748b9ed9eb3c4bd47011622b7b6a
marianafriass <a01709125@tec.mx>
2025-06-12 03:03:44 -0600

    Update ElementoMusical.cpp

--- ElementoMusical.cpp
+++ ElementoMusical.cpp
@@ -1,17 +1,15 @@
 #include "ElementoMusical.h"
 
 ElementoMusical::ElementoMusical(string titulo, int duracion, int anio) {
-    this->titulo = titulo;
-    this->duracion = duracion;
-    this->anio = anio;
+    this->titulo = titulo; //asignamos el titulo de la canion
+    this->duracion = duracion; //junto con su duracion
+    this->anio = anio; //y año de lanzamiento
 }
-
+//getters
 string ElementoMusical::getTitulo() {return titulo;}
-
 int ElementoMusical::getDuracion() {return duracion;}
 int ElementoMusical::getAnio() {return anio;}
-
+//Setters
 void ElementoMusical::setTitulo(string titulo) {this->titulo = titulo;}
 void ElementoMusical::setDuracion(int duracion) {this->duracion = duracion;}
-
 void ElementoMusical::setAnio(int anio) {this->anio = anio;}
