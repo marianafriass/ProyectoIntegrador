4082f52a1e8af6b4e6e50e7ac5c5988d0fd63f5e
marianafriass <a01709125@tec.mx>
2025-06-12 02:54:08 -0600

    Update Artista.cpp

--- Artista.cpp
+++ Artista.cpp
@@ -1,7 +1,7 @@
 #include "Artista.h"
 
 Artista::Artista(string nombre) {this->nombre = nombre;}
-
+// getters
 string Artista::getNombre() {return nombre;}
-
+//setters
 void Artista::setNombre(string nombre) {this->nombre = nombre;}
