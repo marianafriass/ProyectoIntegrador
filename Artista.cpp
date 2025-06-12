ef499a2a0d0421dfdde341eea329856963d32307
marianafriass <a01709125@tec.mx>
2025-06-12 01:36:11 -0600

    Update Artista.cpp

--- Artista.cpp
+++ Artista.cpp
@@ -1,27 +1,7 @@
 #include "Artista.h"
 
-Artista::Artista() : nombre(""), genero("") {}
+Artista::Artista(string nombre) {this->nombre = nombre;}
 
-Artista::Artista(string nombre, string genero) 
-    : nombre(nombre), genero(genero) {}
+string Artista::getNombre() {return nombre;}
 
-bool Artista::operator==(const Artista& otro) const {
-    return nombre == otro.nombre && genero == otro.genero;
-}
-
-bool Artista::operator!=(const Artista& otro) const {
-    return !(*this == otro);
-}
-
-// Método mostrarInfo
-string Artista::getNombre() const { return nombre; }
-void Artista::setNombre(string nombre) {
-    this->nombre = nombre;
-}
-string Artista::getGenero() const {
-    return genero;
-}
-void Artista::setGenero(string genero) { this->genero = genero; }
-string Artista::mostrarInfo() const {
-    return "Artista: " + nombre + ", Género: " + genero;
-}
+void Artista::setNombre(string nombre) {this->nombre = nombre;}
