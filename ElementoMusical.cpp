086118d892b12034e8b74004f7e146d4e2ab966d
marianafriass <a01709125@tec.mx>
2025-06-12 01:33:57 -0600

    Update ElementoMusical.cpp

--- ElementoMusical.cpp
+++ ElementoMusical.cpp
@@ -1,25 +1,17 @@
 #include "ElementoMusical.h"
 
-ElementoMusical::ElementoMusical() : titulo(""), duracion(0), anio(0) {}
-
-ElementoMusical::ElementoMusical(string titulo, int duracion, int anio)
-    : titulo(titulo), duracion(duracion), anio(anio) {}
-
-string ElementoMusical::getTitulo() const {
-    return titulo;
-}
-void ElementoMusical::setTitulo(string titulo) {
+ElementoMusical::ElementoMusical(string titulo, int duracion, int anio) {
     this->titulo = titulo;
-}
-int ElementoMusical::getDuracion() const {
-    return duracion;
-}
-void ElementoMusical::setDuracion(int duracion) {
     this->duracion = duracion;
-}
-int ElementoMusical::getAnio() const {
-    return anio;
-}
-void ElementoMusical::setAnio(int anio) {
     this->anio = anio;
 }
+
+string ElementoMusical::getTitulo() {return titulo;}
+
+int ElementoMusical::getDuracion() {return duracion;}
+int ElementoMusical::getAnio() {return anio;}
+
+void ElementoMusical::setTitulo(string titulo) {this->titulo = titulo;}
+void ElementoMusical::setDuracion(int duracion) {this->duracion = duracion;}
+
+void ElementoMusical::setAnio(int anio) {this->anio = anio;}
