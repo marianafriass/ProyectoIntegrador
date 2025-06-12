ba81903ee69bc98e646c30822cfe3ed7b3ff3e23
marianafriass <a01709125@tec.mx>
2025-06-12 01:35:55 -0600

    Update Artista.h

--- Artista.h
+++ Artista.h
@@ -7,22 +7,11 @@ using namespace std;
 class Artista {
 private:
     string nombre;
-    string genero;
 
 public:
-    Artista();
-    Artista(string nombre, string genero);
-    
-    // Métodos
-    string getNombre() const;
+    Artista(string nombre = "");
+    string getNombre();
     void setNombre(string nombre);
-    string getGenero() const;
-    void setGenero(string genero);
-    string mostrarInfo() const;
-
-    // Operadores sobrecargados eliminados
-    bool operator==(const Artista& otro) const;
-    bool operator!=(const Artista& otro) const;
 };
 
 #endif
